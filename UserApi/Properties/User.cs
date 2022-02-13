6f22f5d6694db796178cc7eb4897a341cd8b308c
4lbatr0s <serhatoner@protonmail.com>
2022-02-13 20:42:13 +0300

    create .net6 minimal api & unit tests

---
+++ UserApi/Properties/User.cs
@@ -0,0 +1,13 @@
+namespace UserApi.Properties
+{
+    public class User
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public string? Surname {get; set;}
+            public string? Gender {get; set;}
+            public string? BirthDate {get; set;}
+            public string? CreatedAt {get; set;}
+        }
+
+}
